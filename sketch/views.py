2763ae7d6633b5acc6d685422aea82c0615871a0
Sheil Patel <spatel9114@gmail.com>
2013-02-05 17:01:35 -0600

    about page work
    added code in urls.py, views.py, and base.html adding the about page

--- sketch/views.py
+++ sketch/views.py
@@ -1,6 +1,10 @@
-from flask.views import MethodView
+from flask.views import MethodView, View
 from flask.templating import render_template
 
 class SketchView(MethodView):
     def get(self):
         return render_template('sketch.html')
+		
+class AboutView(View):
+	def get(self):
+		return render_templage('about.html')
