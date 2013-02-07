7f143ff0ceb35aa6fe85e69b42520d0e1e19a146
Theisen Sanders <theisen.sanders@gmail.com>
2013-02-07 01:31:12 -0600

    Further Facebook implementation

--- auth/models.py
+++ auth/models.py
@@ -5,12 +5,24 @@ from resources.flask_login import AnonymousUser
 
 class User(db.Model):
     username = db.StringProperty()
+    name = db.StringProperty()
     password = db.StringProperty()
     salt = db.StringProperty()
     email = db.EmailProperty()
     created = db.DateTimeProperty(auto_now=True)
-#    facebook_id = db.StringProperty()
-#    https://developers.google.com/appengine/docs/python/datastore/typesandpropertyclasses
+    facebook_id = db.StringProperty()
+    # https://developers.google.com/appengine/docs/python/datastore/typesandpropertyclasses
+
+    @property
+    def display_name(self):
+        if self.name:
+            return self.name
+
+        elif self.username:
+            return self.username
+
+        else:
+            return self.email
 
     def get_id(self):
         return self.key().id()
@@ -24,5 +36,10 @@ class User(db.Model):
     def is_authenticated(self):
         return True
 
+
 class Anonymous(AnonymousUser):
     username = "Anonymous"
+
+    @property
+    def display_name(self):
+        return self.username
