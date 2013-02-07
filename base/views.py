3e8f6ab0259fed9d187e17cabbca74d43a12e228
Sheil Patel <spatel9114@gmail.com>
2013-02-07 09:36:05 -0600

    about page
    the about page is pretty much complete at this point, may add a few
    other details

--- base/views.py
+++ base/views.py
@@ -1,7 +1,10 @@
-from flask.views import MethodView
+from flask.views import MethodView, View
 from flask.templating import render_template
 
 class HomeView(MethodView):
     def get(self):
         return render_template('home.html' )
 
+class AboutView(View):
+	def dispatch_request(self):
+		return render_template('about.html' )
