b9157d833806c5a4c8e85f8c365f432770931dac
Sheil Patel <spatel9114@gmail.com>
2013-02-07 09:36:05 -0600

    about page
    the about page is pretty much complete at this point, may add a few
    other details

--- base/urls.py
+++ base/urls.py
@@ -2,3 +2,4 @@ from base import views as base_views
 
 def apply_urls(app):
     app.add_url_rule('/', view_func=base_views.HomeView.as_view('home'))
+    app.add_url_rule('/about', view_func=base_views.AboutView.as_view('about'))
