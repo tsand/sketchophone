19a331b26fa594b5af76cf75aff5a388b63b0b3d
Theisen Sanders <theisen.sanders@gmail.com>
2013-02-12 10:55:54 -0600

    Edge case checking, email templating

--- base/cron.py
+++ base/cron.py
@@ -2,10 +2,6 @@ from google.appengine.ext import deferred
 
 from auth import actions as auth_actions
 
+
 def clean_users():
     deferred.defer(auth_actions.delete_expired_users)
-    return 'TEST'
-
-
-def run(function):
-    function_call = getattr()
