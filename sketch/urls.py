8ae1f5d0fc0fb2c053317fea8ae6b1649f38175d
Robbie Sheehy <gameboy1092@gmail.com>
2013-02-26 11:47:52 -0600

    Added new search page to our website!

--- sketch/urls.py
+++ sketch/urls.py
@@ -3,4 +3,5 @@ from sketch import views as sketch_views
 def apply_urls(app):
     app.add_url_rule('/sketch/', view_func=sketch_views.SketchView.as_view('sketch'))
     app.add_url_rule('/sketch/create/', view_func=sketch_views.CreationWizard.as_view('wizard'))
-    
+    app.add_url_rule('/sketch/search/', view_func=sketch_views.SearchGamesView.as_view('search'))
+    # app.add_url_rule('/sketch/filter/', view_func=sketch_views.FilterGamesView.as_view('filter_games'))
